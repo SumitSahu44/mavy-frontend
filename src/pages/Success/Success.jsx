1a6e5aa857a8ee130c72d41ba449f3ac04af2131
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-03 01:20:30 +0530

    add navbar in success page and make changes on it

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,12 +1,66 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { IoCheckmarkDoneCircle } from "react-icons/io5";
+import { useGSAP } from "@gsap/react";
+import { FiShoppingCart } from "react-icons/fi"; // Import cart icon
+import { gsap, Power3, Circ, Expo } from 'gsap';
+import { RiMenu3Fill } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
+import Slider from 'react-slick';
+import { ThreeDots } from 'react-loader-spinner';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Success.css';
+import '../Home/home.css'
+
 const PaymentSuccess = () => {
 
     const [waitingTime, setwaitingTime] = useState(8)
     const authToken = localStorage.getItem('authToken'); // Retrieve token from localStorage
     const [isValid, setIsValid] = useState(false);
+    var cartLength;
+    
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    cartLength = cartItems.length;
+ 
+
+    useGSAP(() => {
+        let menu = document.querySelector("#nav i");
+        let close = document.querySelector("#full i");
+
+        let first = gsap.timeline()
+
+        first.to("#full", {
+            right: 0,
+            duration: 0.5
+        })
+
+        first.from("#full h4", {
+            x: 150,
+            duration: 0.6,
+            stagger: 0.2,
+            opacity: 0
+        })
+
+        first.from("#full i", {
+            opacity: 0
+        })
+
+
+        first.pause()
+
+
+        menu.addEventListener("click", function () {
+            first.play()
+        })
+
+        close.addEventListener("click", function () {
+            first.reverse()
+        })
+    })
+
+
+
+
     useEffect(() => {
         const checkPaymentStatus = async () => {
             const queryParams = new URLSearchParams(window.location.search);
@@ -14,47 +68,47 @@ const PaymentSuccess = () => {
             const email = queryParams.get('email');
          
             console.log('session id inside checkpaymnent',sessionId)
-            if(!sessionId)
-            {
-                window.location.href = './'
-            }
-              try {
-                    const response = await fetch(`https://mavy-pxtx.onrender.com/user/checkout?session_id=${sessionId}`);
-                    const data = await response.json();
+            // if(!sessionId)
+            // {
+            //     window.location.href = './'
+            // }
+            //   try {
+            //         const response = await fetch(`https://mavy-pxtx.onrender.com/user/checkout?session_id=${sessionId}`);
+            //         const data = await response.json();
     
                    
-                    console.log(data)
-                    if (data.success) {
-                        setIsValid(true);
+            //         // console.log(data)
+            //         if (data.success) {
+            //             setIsValid(true);
 
-                        const mailResponse = await fetch('https://mavy-pxtx.onrender.com/user/checkout', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            //    'Authorization': `Bearer ${authToken}`, // Attach token in the Authorization header
-                              },  
-                            credentials: 'include', // Include cookies
-                            body: JSON.stringify({
-                                email, // Send the email from input
+            //             const mailResponse = await fetch('https://mavy-pxtx.onrender.com/user/checkout', {
+            //                 method: 'POST',
+            //                 headers: {
+            //                     'Content-Type': 'application/json',
+            //                 //    'Authorization': `Bearer ${authToken}`, // Attach token in the Authorization header
+            //                   },  
+            //                 credentials: 'include', // Include cookies
+            //                 body: JSON.stringify({
+            //                     email, // Send the email from input
                              
-                            })
-                        });
+            //                 })
+            //             });
 
-                        if(mailResponse)
-                        {
-                            localStorage.removeItem("cart");
-                            localStorage.clear();
+            //             if(mailResponse)
+            //             {
+            //                 localStorage.removeItem("cart");
+            //                 localStorage.clear();
                             
-                             console.log("Cart cleared from localStorage.");
-                        }
-                    } 
-                    else {
-                       window.location.href="./"
-                    }
-                } catch (error) {
-                    console.error("Session validation error:", error);
-                    navigate("/"); // Redirect on any error
-                }
+            //                  console.log("Cart cleared from localStorage.");
+            //             }
+            //         } 
+            //         else {
+            //            window.location.href="./"
+            //         }
+            //     } catch (error) {
+            //         console.error("Session validation error:", error);
+            //         navigate("/"); // Redirect on any error
+            //     }
                       
     
                   
@@ -64,22 +118,38 @@ const PaymentSuccess = () => {
     }, []);
 
 
-       {
-        setTimeout(() => {
-            window.location.href="/product"
-         }, 8000);
-      } 
-
-       {
-        setInterval(()=>{
-            setwaitingTime(waitingTime - 1)
-        },1000)
-      }
-
+     
     return (
 
   
         <div>
+
+               <header>
+                                <div className="navbar">
+                                    <div id="nav">
+                                        <img src="/img/qt=q_95.jpeg" alt="" />
+                                        <div>
+                                            
+                                            <a href="/cart" style={{ position: "relative", textDecoration: "none", color: "white" }}>
+                                                <FiShoppingCart size={25} style={{ margin: "5px 10px" }} />
+                                                <sup style={{position:"absolute",}}>{cartLength?cartLength:''}</sup>
+                                            </a>
+                                            <i style={{marginLeft:"20px"}} ><RiMenu3Fill /></i>
+                                         </div>
+                                       </div>
+                                    <div id="full">
+                                        <a href="#"><h4>Home</h4></a>
+                                        <a href="/about"><h4>About</h4></a>
+                                        <a href="/product"><h4>Products</h4></a>
+                                        <a href="/cart"><h4>Cart</h4></a>
+                                        {/* <a href="/login"><h4>Profile</h4></a> */}
+                                        <i><IoMdClose /></i>
+                                    </div>
+                                </div>
+                            </header>
+
+
+
      {/* <IoCheckmarkDoneCircle className='success-icon' /> */}
             <div className="container">
                 <div className="row justify-content-center">
@@ -87,18 +157,17 @@ const PaymentSuccess = () => {
                         <div className="message-box _success">
                             <i className="fa fa-check-circle" aria-hidden="true"></i>
                             <h2> Your payment was successful </h2>
-                            <p>Thank you for choosing Mavy Scrubs! Each piece is handmade with care in Ghana using authentic kente fabric. Please allow 30-45 business days for delivery due to the handmade process and international shipping.
+                            <p style={{"text-align":"justify"}}>Thank you for choosing Mavy Scrubs! Each piece is handmade with care in Ghana using authentic kente fabric. Please allow 30-45 business days for delivery due to the handmade process and international shipping.
 
-                                <br />
-                                We appreciate your patience and support!
-                                <br />
-
-                                Mavy Scrubs Team
+                             
                             </p>
-                                <br />
-                             <small>Don't Refres page or Click back button</small>
-                             <br />
-                             <h4 className='redirect'>We Redirect you to Home page {waitingTime} secound</h4>  
+                            <br />              
+                                <p style={{"text-align":"justify"}}> 
+                                We appreciate your patience and support!
+                                 Mavy Scrubs Team</p>
+
+                              <br />
+                             <h4 className='redirect'><a href="/">Back to MavyScrubs</a></h4>  
                         </div>
                     </div>
                 </div>
